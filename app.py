9ea3544671e407de5339b051443581c52b460d8a
Joshua Cook <me@joshuacook.me>
2016-08-19 20:50:40 -0700

    refactor and abstract app.py

--- app.py
+++ app.py
@@ -1,91 +1,39 @@
 # Import libraries
 import numpy as np
 import pandas as pd
-import os
-import time
-# from matplotlib import pyplot as plt
 from sklearn import grid_search
-from sklearn.metrics import f1_score
-from sklearn import tree, svm, naive_bayes, ensemble, neighbors
 from sklearn.cross_validation import train_test_split
-import datetime
 
-# Read student data
-student_data = pd.read_csv("student-data.csv")
-student_data.reindex(np.random.permutation(student_data.index))
-print("Data read successfully!")
+from lib.classifiers import CLASSIFIERS
+from lib.model import Model
 
-dtc = tree.DecisionTreeClassifier()
-svc = svm.SVC()
-nbc = naive_bayes.GaussianNB()
-knn = neighbors.KNeighborsClassifier()
-rfc = ensemble.RandomForestClassifier()
-adc = ensemble.AdaBoostClassifier()
+data = np.load('./tmp/testTrainData.npz')
+print(data.keys())
+X_train, X_test, y_test, y_train = (data[item] for item in data.keys())
 
-models = [dtc, svc, nbc, knn, rfc, adc]
-X, y = np.arange(1000).reshape((500, 2)), range(500)
-
-class Model:
-    def __init__(self, classifier, parameters=[]):
-        self.classifier = classifier
-        self.parameters = parameters
-
-    def train_classifier(self, clf, X_train, y_train):
-        print("Training {}...".format(clf.__class__.__name__))
-         # start = np.datetime64(datetime.datetime.now(),"us")
-        start = time.time()
-        clf.fit(X_train, y_train)
-        # end = np.datetime64(datetime.datetime.now(),"us")
-        end = time.time()
-        self.training_time = end - start
-        print(self.training_time)
-
-    def predict_labels(self, clf, features, target):
-        # print("Predicting labels using {}...".format(clf.__class__.__name__))
-         # start = np.datetime64(datetime.datetime.now(),"us")
-        start = time.time()
-        y_pred = clf.predict(features)
-        # end = np.datetime64(datetime.datetime.now(),"us")
-        end = time.time()
-        self.prediction_time = end - start
-        f1_score_output = f1_score(target, y_pred, average="macro")
-        return f1_score_output
-
-    def train_predict(self, clf, X_train, y_train, X_test, y_test):
-        print("------------------------------------------")
-        print("Training set size: {}".format(len(X_train)))
-        self.train_classifier(clf, X_train, y_train)
-        self.f1_train = self.predict_labels(clf, X_train, y_train)
-        self.f1_test = self.predict_labels(clf, X_test, y_test)
-        return [self.training_time, self.prediction_time, self.f1_train, self.f1_test]
-
-
+classifiers = []
 dataframes = []
+train_times = []
+pred_times = []
+f1_trains = []
+f1_tests = []
 
-for x in [100, 200, 300]:
-    classifiers = []
-    train_times = []
-    pred_times = []
-    f1_trains = []
-    f1_tests = []
+for classifier, parameters in CLASSIFIERS:
+    this_model = Model(classifier, parameters)
 
-    for model in models:
-        clf = Model(model)
-        X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.33, random_state=42)
+    this_model(X_train, y_train, X_test, y_test)
+    classifiers.append(this_model.classifier.__class__.__name__)
+    train_times.append(this_model.training_time)
+    pred_times.append(this_model.train_prediction_time)
+    f1_trains.append(this_model.f1_train)
+    f1_tests.append(this_model.f1_test)
 
-        output = clf.train_predict(model, X_train, y_train, X_test, y_test)
-        classifiers.append(model.__class__.__name__)
-        train_times.append(str(output[0]))
-        pred_times.append(str(output[1]))
-        f1_trains.append(output[2])
-        f1_tests.append(output[3])
-
-    df = pd.DataFrame({"Classifier": classifiers,
-                       "Training Time": train_times,
-                       "Prediction Time": pred_times,
-                       "F1 Score on Training Set": f1_trains,
-                       "F1 Score on Test Set": f1_tests})
-    dataframes.append(df)
+df = pd.DataFrame({"Classifier": classifiers,
+                   "Training Time": train_times,
+                   "Prediction Time": pred_times,
+                   "F1 Score on Training Set": f1_trains,
+                   "F1 Score on Test Set": f1_tests})
+dataframes.append(df)
 
 for i, frame in enumerate(dataframes):
     filenumber = i * 100
