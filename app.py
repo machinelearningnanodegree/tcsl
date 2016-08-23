931f5416df34f087a5519b197f116f6ae6db7596
David Timm <timmmeister@gmail.com>
2016-08-23 12:30:12 -0600

    Implement grid search for all classifiers.

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 # Import libraries
 import numpy as np
 import pandas as pd
-from sklearn import grid_search
-from sklearn.cross_validation import train_test_split
 
 from lib.data.wrangler import readData
 from lib.helpers import runTests, writeToCsv
 
-X_test, \
-    X_train, \
+X_train, \
+    X_test, \
     y_test, \
     y_train = readData('./tmp/testTrainData.npz')
 
@@ -16,6 +14,7 @@ classifiers, \
     train_times, \
     pred_times, \
     f1_trains, \
-    f1_tests = runTests(X_test, X_train, y_test, y_train)
+    f1_tests, \
+    gs_times = runTests(X_test, X_train, y_test, y_train)
 
-writeToCsv(classifiers, train_times, pred_times, f1_trains, f1_tests)
+writeToCsv(classifiers, train_times, pred_times, f1_trains, f1_tests, gs_times)
--- lib/helpers/__init__.py
+++ lib/helpers/__init__.py
@@ -9,32 +9,46 @@ from lib.model.classifiers import CLASSIFIERS
 def runTests(X_test, X_train, y_test, y_train):
     classifiers = []
     train_times = []
+    gs_times = []
     pred_times = []
     f1_trains = []
     f1_tests = []
 
-    for classifier, parameters in CLASSIFIERS:
-        this_model = Model(classifier, parameters)
+    for classifier, parameters, gs_params in CLASSIFIERS:
+        this_model = Model(classifier, parameters, gs_params)
 
         this_model(X_train, y_train, X_test, y_test)
-        classifiers.append(this_model.classifier.__class__.__name__)
+
+        # Append vanilla values
+        classifiers.append(this_model.classifier.__name__)
         train_times.append(this_model.training_time)
         pred_times.append(this_model.train_prediction_time)
         f1_trains.append(this_model.f1_train)
         f1_tests.append(this_model.f1_test)
+        gs_times.append(0)
 
-    return classifiers, train_times, pred_times, f1_trains, f1_tests
+        # Append optimized classifier values
+        classifiers.append(this_model.classifier.__name__ + ' (Optimized)')
+        train_times.append(this_model.optimal_training_time)
+        pred_times.append(this_model.optimal_train_prediction_time)
+        f1_trains.append(this_model.f1_optimal_train)
+        f1_tests.append(this_model.f1_optimal_test)
+        gs_times.append(this_model.gs_time)
 
-def writeToCsv(classifiers, train_times, pred_times, f1_trains, f1_tests):
+
+    return classifiers, train_times, pred_times, f1_trains, f1_tests, gs_times
+
+def writeToCsv(classifiers, train_times, pred_times, f1_trains, f1_tests, gs_times):
     df = pd.DataFrame({"Classifier": classifiers,
                        "Training Time": train_times,
                        "Prediction Time": pred_times,
                        "F1 Score on Training Set": f1_trains,
-                       "F1 Score on Test Set": f1_tests})
+                       "F1 Score on Test Set": f1_tests,
+                       "Grid Search Time":gs_times})
 
     t = datetime.datetime(2011, 10, 21, 0, 0)
     t = time.mktime(t.timetuple())
     if not os.path.exists('results'):
         os.makedirs('results')
     filename = str(t)+'.csv'
-    frame.to_csv('results/'+filename)
+    df.to_csv('results/'+filename)
